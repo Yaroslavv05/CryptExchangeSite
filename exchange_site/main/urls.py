53b0a5f28c9abb8a55f913c7e3909e4bc109ce65
Limb <88242725+Yaroslavv05@users.noreply.github.com>
2022-10-03 15:08:38 +0300

    update

--- exchange_site/main/urls.py
+++ exchange_site/main/urls.py
@@ -4,5 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('questions', questions_and_answers, name='questions'),
+    path('rules', rules, name='rules'),
+    path('contacts', contact, name='contacts'),
+    path('en', index, name='english'),
     path('create', create, name='create'),
 ]
--- exchange_site/main/views.py
+++ exchange_site/main/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
-
 def index(request):
     return render(request, 'main/NETEX 24.html')
 
 
 def create(request):
     return render(request, 'main/create.html')
+
+
+def questions_and_answers(request):
+    return render(request, 'main/Questions and answers.html')
+
+
+def rules(request):
+    return render(request, 'main/Rules.html')
+
+
+def contact(request):
+    return render(request, 'main/Contact.html')
