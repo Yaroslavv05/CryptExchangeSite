9eeb2a36690c8e416e863e17e81214bb1985c48e
Limb <88242725+Yaroslavv05@users.noreply.github.com>
2022-10-14 21:26:31 +0300

    update

--- exchange_site/main/views.py
+++ exchange_site/main/views.py
@@ -37,6 +37,7 @@ def index(request):
     if request.method == 'POST':
         form = Form(request.POST)
         if form.is_valid():
+            '''''
             mail = send_mail("+новые бабки",
                              f"""
                              Фио: {form.cleaned_data['fio']}
@@ -45,10 +46,10 @@ def index(request):
                              Название и количевство: {coins[form.cleaned_data['coin_name']]}/{form.cleaned_data['colvo_coin']}
                              """
                              , EMAIL_HOST_USER, ['chornyyaroslav5@gmail.com'], fail_silently=False)
+            '''''
             request.session['count'] = form.cleaned_data['colvo_coin']
             request.session['coin_name'] = form.cleaned_data['coin_name']
             form.save()
-            print(mail)
             return redirect('create')
         else:
             messages.error(request, 'Ошибка валидации')
